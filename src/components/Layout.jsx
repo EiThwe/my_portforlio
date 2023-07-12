5873337c982c275425fc436bc1bf916a67782e76
Denny_Kate <dennykate22@gmail.com>
2023-07-12 22:57:08 +0630

    fixed Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+
 import Cursor from "./Cursor";
 
 const Layout = ({ children }) => {
+  const [showSidebar, setShowSidebar] = useState(false);
+
   return (
     <div>
       <Cursor />
@@ -9,14 +12,12 @@ const Layout = ({ children }) => {
         <div
           className="w-[95%] sm:h-[90%] h-[95%] bg-[#ebf6f7] backdrop-blur-[3px] bg-opacity-5 rounded-xl shadow-inner	
           shadow-[#6b6b6b] border border-gray-500 border-opacity-20 flex justify-center items-center overflow-hidden
-           relative"
+           relative py-[10px] "
         >
-          <div className="h-full w-[40%] "></div>
-          <div className="min-h-full w-[60%] ">{children}</div>
-        </div>
-
-        <div className="absolute top-0 left-0 w-full h-full flex justify-end bg-[#000] bg-opacity-30">
-          <div className="h-full w-[30%] bg-[#ebf6f7] backdrop-blur-[7px] bg-opacity-5"></div>
+          <div className="h-full w-[50%] "></div>
+          <div className="h-full w-[50%]  overflow-y-auto">
+            <div className="w-[70%]">{children}</div>
+          </div>
         </div>
       </div>
     </div>
