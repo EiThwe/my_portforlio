75f51acd0ff466df183f1f4c517e07fa3f8fc54b
thwe thwe tun <thwethwehtun2000@gmail.com>
2023-07-15 00:13:51 +0630

    color finding

--- src/components/ChatShortcutButton.jsx
+++ src/components/ChatShortcutButton.jsx
@@ -7,19 +7,19 @@ const ChatShortcutButton = () => {
   const [show, setShow] = useState(false);
 
   return (
-    <div className="w-[300px] h-[300px] fixed -bottom-[70px] -right-[70px]">
+    <div className="w-[250px] h-[250px] fixed -bottom-[70px] -right-[70px]">
       <div className="w-full h-full relative flex justify-center items-center">
         {show ? (
           <button
             onClick={() => setShow(!show)}
-            className="chat-shortcut w-14 h-14 z-10 rounded-full bg-white backdrop-blur-[10px] bg-opacity-10 flex justify-center items-center shadow-[#6b6b6b] shadow-inner"
+            className="chat-shortcut w-14 h-14 z-10 rounded-full bg-primary backdrop-blur-[10px] bg-opacity-10 flex justify-center items-center shadow-[#6b6b6b] shadow-inner"
           >
             <X size={30} className="text-white" />
           </button>
         ) : (
           <button
             onClick={() => setShow(!show)}
-            className="chat-shortcut w-14 h-14 z-10 rounded-full bg-white backdrop-blur-[10px] bg-opacity-10 flex justify-center items-center shadow-[#6b6b6b] shadow-inner"
+            className="chat-shortcut w-14 h-14 z-10 rounded-full border border-white border-opacity-30 bg-primary backdrop-blur-[10px] bg-opacity-10 flex justify-center items-center shadow-[#6b6b6b] shadow-inner"
           >
             <HiOutlineChatBubbleOvalLeftEllipsis
               size={30}
@@ -29,20 +29,20 @@ const ChatShortcutButton = () => {
         )}
 
         <div
-          className={`absolute w-full h-full bg-[#6C6C6C] bg-opacity-20 backdrop-blur-[3px]  rounded-full z-[1] ${
+          className={`absolute w-full h-full  bg-primary bg-opacity-20 backdrop-blur-[3px]  rounded-full z-[1] ${
             show ? "scale-100" : "scale-0"
           } transition-all duration-300 ease-in-out`}
         >
           <Link
             to={"https://t.me/eT2919"}
             target="_blank"
-            className="chat-shortcut absolute top-[120px] left-[10px] w-11 h-11 z-10 rounded-full bg-white backdrop-blur-[20px] bg-opacity-10 flex justify-center items-center shadow-[#6b6b6b] shadow-inner"
+            className="chat-telegram absolute top-[120px] left-[10px] w-11 h-11 z-10 rounded-full bg-primary backdrop-blur-[20px] bg-opacity-10 flex justify-center items-center shadow-[#6b6b6b] shadow-inner"
           >
             <BrandTelegram size={25} strokeWidth={1.2} className="text-white" />
           </Link>
           <Link
             to={"https://m.me/100008563840418"}
-            className="chat-shortcut absolute top-[40px] left-[40px] w-11 h-11 z-10 rounded-full bg-white backdrop-blur-[20px] bg-opacity-10 flex justify-center items-center shadow-[#6b6b6b] shadow-inner"
+            className="chat-messenger absolute top-[40px] left-[40px] w-11 h-11 z-10 rounded-full bg-primary backdrop-blur-[20px] bg-opacity-10 flex justify-center items-center shadow-[#6b6b6b] shadow-inner"
           >
             <BrandMessenger
               size={25}
@@ -53,7 +53,7 @@ const ChatShortcutButton = () => {
           <Link
             to={""}
             onClick={() => setShow(!show)}
-            className="chat-shortcut absolute top-[10px] left-[120px] w-11 h-11 z-10 rounded-full bg-white backdrop-blur-[20px] bg-opacity-10 flex justify-center items-center shadow-[#6b6b6b] shadow-inner"
+            className="own-chat absolute top-[10px] left-[120px] w-11 h-11 z-10 rounded-full bg-primary backdrop-blur-[20px] bg-opacity-10 flex justify-center items-center shadow-[#6b6b6b] shadow-inner"
           >
             <HiOutlineChatBubbleOvalLeftEllipsis
               size={25}
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,39 +4,57 @@ import { BsBoxArrowUpRight } from "react-icons/bs";
 import { Link } from "react-router-dom";
 const ProjectCard = ({ image, title, link, tools }) => {
   const ref = useRef();
-  const [height, setHeight] = useState(null);
+
   const [loading, setLoading] = useState(true);
   const [hover, setHover] = useState(false);
 
-  useEffect(() => {
-    setHeight(`translate-y-[${ref.current.clientHeight}px]`);
-    console.log(height);
-  }, []);
+  // useEffect(() => {
+  //   setHeight(`translate-y-[${ref.current.clientHeight}px]`);
+  //   console.log(height);
+  // }, [height]);
   return (
-    <>
+    <div>
       <div
         onMouseOver={() => setHover(true)}
         onMouseLeave={() => setHover(false)}
-        className=" project-card w-full h-[260px] bg-gray-400 flex justify-center items-center relative shadow-inner overflow-hidden"
+        className=" project-card w-full h-[250px] bg-[#ebf6f7] backdrop-blur-[3px] bg-opacity-5 rounded-xl shadow-inner	
+        shadow-[#6b6b6b] border border-gray-500 border-opacity-20 flex justify-center items-center overflow-hidden z-[100]"
       >
-        {loading && <div className="w-full h-full bg-gray-400"></div>}
-        <img
-          onLoad={() => setLoading(false)}
-          src={image}
-          alt=""
-          className="project-img w-full h-full bg-black"
-        />
+        {/* loading section */}
+        {loading && (
+          <div>
+            <div className="w-full h-full bg-gray-400"></div>
+          </div>
+        )}
+        {/* img section */}
+        <div className="img-container">
+          <img
+            onLoad={() => setLoading(false)}
+            src={image}
+            alt="project"
+            className="project-img w-full h-full rounded-xl"
+          />
+        </div>
+
+        {/* cover layer */}
         <div className="w-full h-full absolute bg-black bg-opacity-50"></div>
+
+        {/* content */}
         <div
-          className={`hidden-container absolute bottom-0 w-full px-2 ${
-            hover ? "translate-y-0" : height
-          } transition-all ease-linear duration-300`}
+          className={`hidden-container absolute bottom-0 w-full px-2 transition-all ease-linear duration-300`}
+          style={{
+            transform: hover
+              ? "translateY(0px)"
+              : `translateY(${ref.current?.clientHeight}px)`,
+          }}
         >
+          {/* text */}
           <div className="w-full">
             <h3 className="text-[20px] mb-2 tracking-tight">{title}</h3>
             <hr className={`${hover ? "w-16" : "w-8"} mb-3 `} />
           </div>
 
+          {/* tools */}
           <div className={`flex justify-between w-full`} ref={ref}>
             <div className=" flex flex-wrap w-[70%] gap-1 mb-2">
               {tools.map(({ title }, i) => (
@@ -52,6 +70,7 @@ const ProjectCard = ({ image, title, link, tools }) => {
                 </Badge>
               ))}
             </div>
+            {/* preview link */}
             <Link
               to={link}
               target="_blank"
@@ -64,7 +83,7 @@ const ProjectCard = ({ image, title, link, tools }) => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
